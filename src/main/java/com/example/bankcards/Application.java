3408e954307f642ef091f44ead6fcaa44d65cf7d
npoleva <poleva.2002@gmail.com>
2025-06-06 00:46:21 +0300

    feat: initialize core application with Docker-ready config
    - Add main @SpringBootApplication class
    - Update DB config for Docker compatibility (SPRING_DATASOURCE_URL)

--- src/main/java/com/example/bankcards/Application.java
+++ src/main/java/com/example/bankcards/Application.java
@@ -3,8 +3,9 @@ package com.example.bankcards;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class Application {
+public class Application  {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
